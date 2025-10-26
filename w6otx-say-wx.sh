5f91a71981cdfd69d863bd0f5973c3858e95977a
Christopher Hoover <ch@murgatroid.com>
2025-10-26 13:04:05 -0700

    .

--- w6otx-say-wx.sh
+++ w6otx-say-wx.sh
@@ -6,16 +6,18 @@ set -o xtrace
 
 NODE=65314
 ICAO="KSJC"
-ENGINE="google"
-#ENGINE="espeak"
+#ENGINE="google"
+ENGINE="espeak"
+ENGINE_ARGS=("-v" "uk-female")
 FORMAT="ulaw"
+STYLE="brief"
 
 . ~/google_cloud_tts_api.shfrag
 
 FILE="$(mktemp "/tmp/tmpXXXXXXXXXX.${FORMAT}")"
 trap '/bin/rm -f "${FILE}"' EXIT SIGINT SIGTERM
 
-cargo run --bin "speak-weather" -- "${ENGINE}" -f aviation -a "${FORMAT}" -o "${FILE}" "${ICAO}"
+speak-weather "${ENGINE}" "${ENGINE_ARGS[@]}" -f "${STYLE}" -a "${FORMAT}" -o "${FILE}" "${ICAO}"
 
 chmod 0644 "${FILE}"
 
